36dedec114335f8e391bdf57b3f8e0627054a2cb
jrob112 <josh.roberts0527@gmail.com>
2024-07-20 11:13:33 -0500

    working on request hanadling

--- client/components/Watch.jsx
+++ client/components/Watch.jsx
@@ -1,17 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import { Eye, EyeSlashFill } from 'react-bootstrap-icons';
 
 function WatchItem() {
   // eye state
   const [showPass, setShowPass] = useState(false);
+  // watcher state
+  const [watchers, setWatchers] = useState([]);
+  // isForSale state
+  const [forSale, setSale] = useState(true);
+
+
+  function getWatchers() {
+    axios
+      .get('/db/watch/${})
+      .then((data) => {
+        console.log(data)
+        // setWatchers(watchers);
+      })
+      .catch((err) => {
+        console.error('Could not GET the watchers', err);
+      });
+  }
+
+  // function sendMessage() {
+  //   watchers.userData.map((user) => {
+  //     axios.post('/messages', {user.name, user.email, watchers.title});
+
+  //   })
+  // };
+
+  // Function to get array of all art objects where isForSale === true
+  function getAuction() {
+    return axios
+      .get('/db/auction/')
+      .then(({ data }) => {
+        setSale(data.isForSale);
+      })
+      .catch((err) => console.error('Could not GET auction items: ', err));
+  }
+
   const clickHandler = () => {
     setShowPass((prev) => !prev);
-    // if (showPass === true && Art.isForSale === true) { sendMessage() }
+    console.log('watchers', watchers)
+    // if (showPass === true && forSale === true) { sendMessage(); }
   };
 
+  useEffect(() => {
+    getAuction();
+    getWatchers();
+  }, [forSale, watchers]);
+
   return (
-    <Button variant="outline" style={{ paddingBottom: '20px' }}>
+    <Button variant='outline' style={{ paddingBottom: '20px' }}>
       {showPass ? (
         <Eye onClick={clickHandler} />
       ) : (
