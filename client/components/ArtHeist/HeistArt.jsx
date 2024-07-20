102b98d53c0dd281343cb441788eb144dba6c6ac
ccaldwell11 <camroncldwll@gmail.com>
2024-07-20 17:03:22 -0500

    Update so modal appears after stealing artwork

--- client/components/ArtHeist/HeistArt.jsx
+++ client/components/ArtHeist/HeistArt.jsx
@@ -4,17 +4,30 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 import { Link } from 'react-router-dom';
 
 function HeistArt({ artwork }) {
+  const [showArt, setShowArt] = useState(false);
+  const [stolen, setStolen] = useState({});
+
   function handleTheft() {
-    axios.post(`/db/stealArt/${artwork._id}`)
+    axios
+      .post(`/db/stealArt/${artwork._id}`)
       .then(({ data }) => {
         console.log('Art stolen', data);
+        setShowArt(true);
+        setStolen(data);
       })
       .catch(() => {
         console.error('Error stealing artwork');
       });
+  }
+
+  function handleClose() {
+    setShowArt(false);
   }
 
   return (
@@ -23,19 +36,37 @@ function HeistArt({ artwork }) {
         <Row>
           <Col className="gallery-item" key={artwork.imageId}>
             <div>
-              <Link to="/home/heist" relative="path">
-                <Image
-                  style={{ width: '250px', height: 'auto' }}
-                  src={artwork.imageUrl}
-                  id={artwork.imageId}
-                  alt={artwork.title}
-                  onClick={() => handleTheft()}
-                />
-              </Link>
+              <Modal
+                show={showArt}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered
+              >
+                <Modal.Header>
+                  <Modal.Title>{`${stolen.title} was added to your collection.`}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  You guessed the entry code to the vault.
+                  Now get out of here before you get caught!
+                </Modal.Body>
+                <Modal.Footer>
+                  <Link to="/home/heist" relative="path">
+                    <Button variant="secondary" onClick={handleClose}>
+                      Leave the scene
+                    </Button>
+                  </Link>
+                </Modal.Footer>
+              </Modal>
+              <Image
+                style={{ width: '250px', height: 'auto' }}
+                src={artwork.imageUrl}
+                id={artwork.imageId}
+                alt={artwork.title}
+                onClick={() => handleTheft()}
+              />
               <br />
-              <div className="gallery-title">
-                {artwork.title}
-              </div>
+              <div className="gallery-title">{artwork.title}</div>
             </div>
             <br />
           </Col>
